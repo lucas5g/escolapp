ff2584855bff08ba33469de9825aa117afffb8eb
lucas5g <lucasdesousa19@hotmail.com>
2023-02-01 23:45:44 -0300

    add auto fixMysql

--- test/cypress/e2e/api/5-games.cy.ts
+++ test/cypress/e2e/api/5-games.cy.ts
@@ -4,7 +4,7 @@ describe('Games', () => {
   let accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6Ikx1Y2FzIGRlIHNvdXNhIGFzc3Vuw6fDo28gMjIiLCJpYXQiOjE2NzUyMTc3NjcsImV4cCI6MTY3NTI2MDk2N30.y36XeIggx_Fab07up5lXnU-QYpi-jzL-Yx1ssB3B8N0"
 
   before(() => {
-    return
+    // return
     cy.login().then(res => accessToken = res.accessToken)
   })
 
