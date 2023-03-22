c52d654c9605f14e988888d044f508ccde696957
lucas5g <lucas.sousa@codificar.com.br>
2023-03-21 23:35:58 -0300

    sem only

--- server/src/tests/course.spec.ts
+++ server/src/tests/course.spec.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from "vitest";
 import { CourseService } from "../services/CourseService";
 
 describe('Course', () => {
-  it.only('list courses', async() => {
+  it('list courses', async() => {
     const courses = await CourseService.findMany()
     expect(courses.length).toBeGreaterThanOrEqual(0)
     expect(courses[0]).toHaveProperty('name')
