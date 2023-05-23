d5469a90a0abf904f3585748630e79ace076f3ec
lucas5g <lucasdesousa19@hotmail.com>
2023-05-23 00:40:09 -0300

    add filter table

--- web/src/components/Table.tsx
+++ web/src/components/Table.tsx
@@ -4,6 +4,8 @@ moment.locale('pt-br');
 
 import { Card, Width } from "./Card";
 import { translate } from "../utils/translate";
+import { Input } from "./Input";
+import { useState } from "react";
 interface Props {
   fields: any[]
   items: any[]
@@ -22,79 +24,91 @@ export function Table({
   width = 100,
   positionBottom = 500
 }: Props) {
-  // console.log(item)
+  const [search, setSearch] = useState('')
+
+  // const test = items?.filter( item => item.name.includes(search))
+  // console.log(test)
+
   return (
+
 
     <Card width={width}>
       {items.length === 0 &&
         <p className="text-gray-500" >Sem registros.</p>
       }
       {items.length > 0 &&
-        <table className="w-full mt-2">
-          <thead>
-            <tr className="border-b font-semibold ">
-              {/* <td className="pb-3 pr-1">N°</td> */}
-              <td className="pb-2">N°</td>
-              {fields.map(head => {
+        <div className="flex flex-col gap-3">
+          {/* <Input
+            label="Pesquisar"
+            name="search"
+            onChange={event => console.log(event.target.value)}
+          /> */}
+          <table className="w-full mt-2">
+            <thead>
+              <tr className="border-b font-semibold ">
+                {/* <td className="pb-3 pr-1">N°</td> */}
+                <td className="pb-2">N°</td>
+                {fields.map(head => {
+                  return (
+                    <td key={head.key} className="pb-2 px-2">
+                      {head.value}
+                    </td>
+                  )
+                })}
+              </tr>
+            </thead>
+            <tbody className="">
+              {items.map((row, index) => {
                 return (
-                  <td key={head.key} className="pb-2 px-2">
-                    {head.value}
-                  </td>
-                )
-              })}
-            </tr>
-          </thead>
-          <tbody className="">
-            {items.map((row, index) => {
-              return (
-                <tr
-                  key={row.id}
-                  className={clsx("border-b hover:bg-blue-50 hover:cursor-pointer transition-colors rounded text-sm", {
-                    'border-b-blue-300 bg-blue-100': row.id === item.id,
-                    // 'text-sm': item.id
-                  })}
-                  title="Clique para Editar"
-                  onClick={() => {
-                    if ('date' in row) {
-                      setItem({ ...row, date: moment(row.date).toDate() })
-                    } else {
-                      setItem(row)
-                    }
-                    const width = document.querySelector('body')?.offsetWidth
-                    if (Number(width) > 1024) {
-                      window.scrollTo({ top: 0, behavior: 'smooth' })
-                    } else {
-                      window.scrollTo({ top: positionBottom, behavior: 'smooth' })
-                      // window.scrollBy({top:1500})
-                    }
-                  }}
-                >
-                  <td className="h-10">
-                    {index + 1}
-                  </td>
-                  {fields.map(field => {
-                    const keyName = field.key.replace('Id', '')
-                    if (field.key === 'date') {
+                  <tr
+                    key={row.id}
+                    className={clsx("border-b hover:bg-blue-50 hover:cursor-pointer transition-colors rounded text-sm", {
+                      'border-b-blue-300 bg-blue-100': row.id === item.id,
+                      // 'text-sm': item.id
+                    })}
+                    title="Clique para Editar"
+                    onClick={() => {
+                      if ('date' in row) {
+                        setItem({ ...row, date: moment(row.date).toDate() })
+                      } else {
+                        setItem(row)
+                      }
+                      const width = document.querySelector('body')?.offsetWidth
+                      if (Number(width) > 1024) {
+                        window.scrollTo({ top: 0, behavior: 'smooth' })
+                      } else {
+                        window.scrollTo({ top: positionBottom, behavior: 'smooth' })
+                        // window.scrollBy({top:1500})
+                      }
+                    }}
+                  >
+                    <td className="h-10">
+                      {index + 1}
+                    </td>
+                    {fields.map(field => {
+                      const keyName = field.key.replace('Id', '')
+                      if (field.key === 'date') {
+                        return (
+                          <td key={field.key}>
+                            {moment(row[field.key]).locale('pt-br').format('DD/MM')}
+                          </td>
+                        )
+                      }
                       return (
-                        <td key={field.key}>
-                          {moment(row[field.key]).locale('pt-br').format('DD/MM')}
+                        <td
+                          className="h-10 px-1"
+                          key={field.key}>
+
+                          {translate(row[keyName]?.['name']) ?? translate(row[field.key])}
                         </td>
                       )
-                    }
-                    return (
-                      <td
-                        className="h-10 px-1"
-                        key={field.key}>
-
-                        {translate(row[keyName]?.['name']) ?? translate(row[field.key])}
-                      </td>
-                    )
-                  })}
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
+                    })}
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
       }
     </Card>
   )
